9b96e4ecfe12ae32e3cb3f85325df88b5ea74af2
vivekananda-reddy <vivekananda-reddy@missionrnd.com>
2016-03-06 19:41:03 +0530

    Submit Commit

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -14,9 +14,51 @@ NOTES: Don't create new array, try to change the input array.
 */
 
 #include <stdio.h>
-
+void shift(int *, int*, int, int);
 int removeArrayDuplicates(int *Arr, int len)
 {
+	int i, j, count, k=-1,a[100];
+	if (Arr == NULL || len < 0)
+	{
+		return -1;
+	}
+	count = len;
+	for (i = 0; i < count; i++)
+	{
+		for (j = i + 1; j < count; j++)
+		{
+			if (Arr[i] == Arr[j])
+			{
+				k++;
+				a[k] = j;
+			}
+		}
+		if (k != -1)
+		{
+			shift(Arr, a, k, count);
+			count = count - k - 1;
+			k = -1;
+		}
+	}
+	return count;
 	
-	return -1;
+}
+void shift(int *Arr, int *a, int k, int count)
+{
+	int i, j=a[0],l=0;
+	
+	for (i = j+1; i < count; i++)
+	{
+		if (l <= k)
+		{
+			if (i == a[l])
+			{
+				l++;
+				continue;
+			}
+		}
+			Arr[j] = Arr[i];
+			j++;
+		
+	}
 }
